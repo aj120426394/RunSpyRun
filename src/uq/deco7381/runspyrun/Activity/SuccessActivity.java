159b2426d885e7da70fec85e71f8da971929fd87
JafoTeng <another0219@gmail.com>
2013-08-18 16:43:50 +1000

    Tutorial of get location: Branch finish

--- src/uq/deco7381/runspyrun/Activity/SuccessActivity.java
+++ src/uq/deco7381/runspyrun/Activity/SuccessActivity.java
@@ -1,39 +1,115 @@
 package uq.deco7381.runspyrun.Activity;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.Menu;
+import android.widget.Toast;
 
 import com.example.runspyrunv3.R;
+import com.google.android.gms.internal.m;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMyLocationChangeListener;
 import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-public class SuccessActivity extends Activity {
+public class SuccessActivity extends Activity  implements android.location.LocationListener {
 
 	static final LatLng NKUT = new LatLng(23.979548, 120.696745);
     private GoogleMap map;
-    
+    private boolean getService = false;
     
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_success);
 		
-		map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
-        Marker nkut = map.addMarker(new MarkerOptions().position(NKUT).title("�n�}��ޤj��").snippet("�Ʀ�ͬ��зN�t"));
-		// Move the camera instantly to NKUT with a zoom of 16.
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(NKUT, 16));
+		LocationManager status = (LocationManager) (this.getSystemService(Context.LOCATION_SERVICE));
+		
+		if (status.isProviderEnabled(LocationManager.GPS_PROVIDER) || status.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+			getService = true;
+			locationServiceInitial();
+			
+		}else{
+			Toast.makeText(this, "Please open the GPS", Toast.LENGTH_LONG).show();
+			startActivity(new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+		}
+		
+		if (map == null){
+			map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+			map.setMyLocationEnabled(true);
+		}
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.success, menu);
-		return true;
+	
+	private LocationManager lms;
+	private String bestProvider = LocationManager.GPS_PROVIDER;	//最佳資訊提供者
+	private void locationServiceInitial() {
+		lms = (LocationManager) getSystemService(LOCATION_SERVICE);	//取得系統定位服務
+		Criteria criteria = new Criteria();	//資訊提供者選取標準
+		bestProvider = lms.getBestProvider(criteria, true);	//選擇精準度最高的提供者
 	}
+	
+
+	@Override
+	protected void onPause() {
+		// TODO Auto-generated method stub
+		super.onPause();
+		if(getService) {
+			lms.removeUpdates((android.location.LocationListener) this);	//離開頁面時停止更新
+		}
+	}
+
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		
+		if(getService){
+			lms.requestLocationUpdates(bestProvider, 1000, 1, this);
+		}
+	}
+
+
+	@Override
+	public void onLocationChanged(Location lastKnowLocation) {
+		// TODO Auto-generated method stub
+		CameraUpdate myLoc = CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder()
+		.target(new LatLng(lastKnowLocation.getLatitude(), lastKnowLocation.getLongitude()))
+		.zoom(15)
+		.build());
+		
+		map.moveCamera(myLoc);
+		map.setOnMyLocationChangeListener(null);
+	}
+
+
+	@Override
+	public void onProviderDisabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onProviderEnabled(String provider) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
