8532cb5bcec8e75f818eaeeb969daba6b0af0df9
JafoTeng <another0219@gmail.com>
2013-08-19 00:52:18 +1000

    Delete lock oreintation

--- src/uq/deco7381/runspyrun/activity/LoginActivity.java
+++ src/uq/deco7381/runspyrun/activity/LoginActivity.java
@@ -32,8 +32,7 @@ public class LoginActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		Parse.initialize(this, "2XLuNz2w0M4iTL5VwXY2w6ICc7aYPZfnr7xyB4EF", "6ZHEiV500losBP4oHmX4f1qVuct1VyRgOlByTVQB");
-		//Screen orientation lock in login screen
-		this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+		
 		ParseAnalytics.trackAppOpened(getIntent());
 		
 		// Get the username for device's storage
