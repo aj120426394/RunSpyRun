1d4b2c404ddeadb35669db7c6868c6e7a2f112d3
JafoTeng <another0219@gmail.com>
2013-08-21 21:24:27 +1000

    fixed bug of loading animation

--- src/uq/deco7381/runspyrun/activity/LoginActivity.java
+++ src/uq/deco7381/runspyrun/activity/LoginActivity.java
@@ -82,6 +82,7 @@ public class LoginActivity extends Activity {
 				// TODO Auto-generated method stub
 				if(user != null){
 					startActivity(intent);
+					showContent();
 				}else{
 					//if there is a exception from parse, show in dialog box
 					builder.setTitle("Alert");
