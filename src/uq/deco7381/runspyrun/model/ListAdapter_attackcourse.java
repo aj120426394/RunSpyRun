db2de3c885d10eb3641cfd7200119cab97f7062a
JafoTeng <another0219@gmail.com>
2013-09-13 03:44:49 +1000

    Start Attack mode, fix the laggy in list view

---
+++ src/uq/deco7381/runspyrun/model/ListAdapter_attackcourse.java
@@ -0,0 +1,37 @@
+package uq.deco7381.runspyrun.model;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+public class ListAdapter_attackcourse extends BaseAdapter {
+
+	public ListAdapter_attackcourse() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public int getCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getItem(int position) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getItemId(int position) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
