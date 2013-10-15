7dd86203f481ea488fd665f8ac250cb9e22a1741
Jonny Lu <johnnyluu@users.noreply.github.com>
2013-10-15 17:57:10 +1000

    slidinglayout

--- src/uq/deco7381/runspyrun/activity/DefenceActivity.java
+++ src/uq/deco7381/runspyrun/activity/DefenceActivity.java
@@ -8,18 +8,16 @@ import uq.deco7381.runspyrun.model.Course;
 import uq.deco7381.runspyrun.model.Guard;
 import uq.deco7381.runspyrun.model.Obstacle;
 import uq.deco7381.runspyrun.model.ParseDAO;
-import android.R.integer;
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -68,10 +66,30 @@ public class DefenceActivity extends Activity implements OnMyLocationChangeListe
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_defence);
+		final ListView listview = (ListView) findViewById(R.id.listview);
 		newObstaclesOnCourse = new ArrayList<Obstacle>();
 		Intent intent = getIntent();
 		dao = new ParseDAO(); 
 		firstLocation = false;
+		
+		String[] values = new String[] { "guard", "detection plate", "dog" };
+
+		    final ArrayList<String> list = new ArrayList<String>();
+		    for (int i = 0; i < values.length; ++i) {
+		      list.add(values[i]);
+		    }
+		    final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, list);
+		        listview.setAdapter(adapter);
+
+		        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+		          @Override
+		          public void onItemClick(AdapterView<?> parent, final View view,
+		              int position, long id) {
+		            final String item = (String) parent.getItemAtPosition(position);
+		            view.animate().setDuration(2000).alpha(0);
+		          }
+		        });
 		
 		/*
 		 * Get the Course's center point (where to put data stream) from intent
